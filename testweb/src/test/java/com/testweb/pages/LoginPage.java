5d48c753609afbf0cd03f59cf767e56c8486e493
NagarjunaMekala <nagarjuna.mekala@copart.com>
2021-07-27 20:20:47 +0530

    comments added

--- testweb/src/test/java/com/testweb/pages/LoginPage.java
+++ testweb/src/test/java/com/testweb/pages/LoginPage.java
@@ -6,12 +6,11 @@ import org.openqa.selenium.support.FindBy;
 
 public class LoginPage {
 	WebDriver driver;
-	
+	//add few more pages
 	public LoginPage(WebDriver driver) {
 		this.driver=driver;
 	}
-	
-	@FindBy(xpath="//input[@name='username1']") WebElement uname;
+		@FindBy(xpath="//input[@name='username1']") WebElement uname;
 	@FindBy(xpath="//input[@name='password']") WebElement upass;
 	@FindBy(xpath="//input[@value='Login']") WebElement logBtn;
 
